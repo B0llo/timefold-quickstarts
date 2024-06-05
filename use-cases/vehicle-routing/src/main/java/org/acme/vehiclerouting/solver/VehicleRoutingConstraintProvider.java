310b940e2e1b3f801062efb7ecf7e8c796cbce6a
Lukas Downes <lukasdownes@gmail.com>
2024-06-05 21:16:08 +0200

    creating constraint

--- use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/solver/VehicleRoutingConstraintProvider.java
+++ use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/solver/VehicleRoutingConstraintProvider.java
@@ -4,12 +4,12 @@ import ai.timefold.solver.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
 import ai.timefold.solver.core.api.score.stream.Constraint;
 import ai.timefold.solver.core.api.score.stream.ConstraintFactory;
 import ai.timefold.solver.core.api.score.stream.ConstraintProvider;
-
-import org.acme.vehiclerouting.domain.Visit;
 import org.acme.vehiclerouting.domain.Vehicle;
+import org.acme.vehiclerouting.domain.Visit;
 import org.acme.vehiclerouting.solver.justifications.MinimizeTravelTimeJustification;
 import org.acme.vehiclerouting.solver.justifications.ServiceFinishedAfterMaxEndTimeJustification;
 import org.acme.vehiclerouting.solver.justifications.VehicleCapacityJustification;
+import org.acme.vehiclerouting.solver.justifications.VehicleLeftAfterMaxLastVisitDepartureTimeJustification;
 
 public class VehicleRoutingConstraintProvider implements ConstraintProvider {
 
@@ -17,11 +17,14 @@ public class VehicleRoutingConstraintProvider implements ConstraintProvider {
     public static final String SERVICE_FINISHED_AFTER_MAX_END_TIME = "serviceFinishedAfterMaxEndTime";
     public static final String MINIMIZE_TRAVEL_TIME = "minimizeTravelTime";
 
+    public static final String VEHICLE_LEFT_AFTER_MAX_LAST_VISIT_DEPARTURE_TIME = "vehicleLeftAfterMaxLastVisitDepartureTime";
+
     @Override
     public Constraint[] defineConstraints(ConstraintFactory factory) {
-        return new Constraint[] {
+        return new Constraint[]{
                 vehicleCapacity(factory),
                 serviceFinishedAfterMaxEndTime(factory),
+                vehicleLeavesAfterMaxLastVisitDepartureTime(factory),
                 minimizeTravelTime(factory)
         };
     }
@@ -49,6 +52,26 @@ public class VehicleRoutingConstraintProvider implements ConstraintProvider {
                         visit.getServiceFinishedDelayInMinutes()))
                 .asConstraint(SERVICE_FINISHED_AFTER_MAX_END_TIME);
     }
+
+    /* TODO 2: WRITE CONSTRAINT
+     *  This constraint has to be a hard constraint, because:
+     *          "A human being CANNOT work without sleep."
+     *  and
+     *          "No vehicle is allowed to depart from its last visit later than the maxLastVisitDepartureTime"
+     *
+     */
+    protected Constraint vehicleLeavesAfterMaxLastVisitDepartureTime(ConstraintFactory factory) {
+        return factory.forEach(Visit.class)
+                .filter(Visit::didVehicleLeaveAfterMaxDepartureTime)
+                // TODO X: DETERMINE PENALTY
+                .penalizeLong(HardSoftLongScore.ONE_HARD,
+                        Visit::getVehicleDepartureDelayInMinutes)
+                .justifyWith((visit, score) -> new VehicleLeftAfterMaxLastVisitDepartureTimeJustification(visit.getId(),
+                        visit.getDepartureTime(),
+                        visit.getVehicle().getMaxLastVisitDepartureTime()))
+                .asConstraint(VEHICLE_LEFT_AFTER_MAX_LAST_VISIT_DEPARTURE_TIME);
+    }
+
 
     // ************************************************************************
     // Soft constraints
